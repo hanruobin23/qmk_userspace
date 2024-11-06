009717b335ab37e6efb2bd65161459a6963d38cc
Ruobin <Ruobin@Ruobins-MacBook-Air.local>
2024-11-06 22:12:46 +0800

    implemented my keymap

---
+++ keyboards/bastardkb/tbkmini/keymaps/hanruobin/rules.mk
@@ -0,0 +1,11 @@
+AUDIO_SUPPORTED = no        # Audio is not supported
+RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
+RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
+
+VIA_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+COMBO_ENABLE = yes
+
+SERIAL_DRIVER = vendor
+CONVERT_TO =  rp2040_ce
