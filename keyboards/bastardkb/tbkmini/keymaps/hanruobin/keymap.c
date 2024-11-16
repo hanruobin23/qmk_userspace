1509b7535e0b06a33beb0e84ab00f1f3bfcb7a09
Ruobin <Ruobin@Ruobins-MacBook-Air.local>
2024-11-16 23:59:59 +0800

    tbas and spaces

--- keyboards/bastardkb/tbkmini/keymaps/hanruobin/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/hanruobin/keymap.c
@@ -70,10 +70,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
  const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_LAYER0] = LAYOUT_split_3x6_3(
-    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,             KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_ESC     ,
-    LALT_T(KC_ESC)  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,             KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT    ,
-    KC_LSFT  ,LT(0,KC_Z),LT(0,KC_X),LT(0,KC_C),LT(0,KC_V), KC_B     ,             KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RGUI    ,
-                     LCTL_T(KC_DEL), KC_ENT   , LT(1,KC_SPC) ,         MO(2)    , KC_SPC   , LT(0,KC_BSPC)
+    KC_TAB          , KC_Q     , KC_W     , KC_E     , KC_R     ,   KC_T     ,             KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_ESC     ,
+    LALT_T(KC_ESC)  , KC_A     , KC_S     , KC_D     , KC_F     ,   KC_G     ,             KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT    ,
+    KC_LSFT         , KC_Z     , KC_X     ,LT(0,KC_C),LT(0,KC_V),   KC_B     ,             KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RGUI    ,
+                                       LCTL_T(KC_DEL),KC_ENT    ,LT(1,KC_SPC),             MO(2)    , KC_SPC   , KC_BSPC
     ),
 
 [_LAYER1] = LAYOUT_split_3x6_3(
