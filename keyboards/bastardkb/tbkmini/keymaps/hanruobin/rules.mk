9ce00a4e56bbf0ccc1fe3545b718cc20032e7de2
Ruobin <Ruobin@Ruobins-MacBook-Air.local>
2024-11-17 00:24:33 +0800

    tabs and spaces

--- keyboards/bastardkb/tbkmini/keymaps/hanruobin/rules.mk
+++ keyboards/bastardkb/tbkmini/keymaps/hanruobin/rules.mk
@@ -3,7 +3,7 @@ RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
 
 VIA_ENABLE = yes
-# TAP_DANCE_ENABLE = yes
+TAP_DANCE_ENABLE = yes
 
 # COMBO_ENABLE = yes
 
