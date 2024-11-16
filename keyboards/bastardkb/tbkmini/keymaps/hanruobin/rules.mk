ce83919eee7b6f25bb0faf7b4c50a09b2a978021
Ruobin <Ruobin@Ruobins-MacBook-Air.local>
2024-11-16 12:35:09 +0800

    basic keymap

--- keyboards/bastardkb/tbkmini/keymaps/hanruobin/rules.mk
+++ keyboards/bastardkb/tbkmini/keymaps/hanruobin/rules.mk
@@ -3,9 +3,8 @@ RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
 
 VIA_ENABLE = yes
-TAP_DANCE_ENABLE = yes
+# TAP_DANCE_ENABLE = yes
 
-COMBO_ENABLE = yes
+# COMBO_ENABLE = yes
 
 SERIAL_DRIVER = vendor
-CONVERT_TO=rp2040_ce
