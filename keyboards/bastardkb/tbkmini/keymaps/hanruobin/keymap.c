80850370bf78c6d23d8a30e69b95ba3f94879f0e
Ruobin <Ruobin@Ruobins-MacBook-Air.local>
2024-11-18 22:19:08 +0800

    modified location of third layer key

--- keyboards/bastardkb/tbkmini/keymaps/hanruobin/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/hanruobin/keymap.c
@@ -142,10 +142,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 [_SYM] = LAYOUT_split_3x6_3(
-    KC_TRNS  , KC_EXLM  , KC_AT    , KC_HASH  , KC_DLR   , KC_PERC  ,             KC_CIRC  , KC_AMPR  , KC_ASTR  , KC_LPRN  , KC_RPRN  , KC_ESC     ,
+    KC_TRNS  , KC_EXLM  , KC_AT    , KC_HASH  , KC_DLR   , KC_PERC  ,             KC_CIRC  , KC_AMPR  , KC_ASTR  , KC_LPRN  , KC_RPRN  , MO(_THIRD) ,
     KC_TRNS  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,             KC_UNDS  , KC_PLUS  , KC_TILD  , KC_LBRC  , KC_RBRC  , KC_PIPE    ,
     KC_TRNS  , KC_6     , KC_7     , KC_8     , KC_9     , KC_0     ,             KC_MINS  , KC_EQL   , KC_GRV   , KC_LCBR  , KC_RCBR  , KC_BSLS    ,
-                                     KC_TRNS  , KC_TRNS  , KC_TRNS  ,             KC_TRNS  , KC_TRNS  , MO(_THIRD)
+                                     KC_TRNS  , KC_TRNS  , KC_TRNS  ,             KC_TRNS  , KC_TRNS  , KC_TRNS
     ),
 
 [_NAV] = LAYOUT_split_3x6_3(
